bc87c0f6ea0153effc5f9a06ba386c39606e5ee6
tracer-ics2015 <tracer@njuics.org>
2015-10-04 03:46:11 -0700

    >  compile NEMU 141220000 fox Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:01 UTC 2015 i686 i686 i686 GNU/Linux  03:46:11 up 10 min,  2 users,  load average: 0.30, 0.39, 0.33 4ac81af842573f4669740562f05b00a18ab2ad78

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -216,7 +216,7 @@ uint32_t  eval(int p,int q)
     }
     else if(check_parentheses(p,q)==true)   /*implement check_parentheses*/
     {
-	printf("()successful");
+	
         return eval(p+1,q-1);
     }
     else
