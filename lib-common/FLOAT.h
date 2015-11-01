8ec519bd685090960b70755c9adb7b3b6cd36a3e
tracer-ics2015 <tracer@njuics.org>
2015-11-01 16:27:16 +0800

    >  compile NEMU 201315035 fox Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  16:27:16 up 21:39,  2 users,  load average: 0.29, 0.12, 0.14 27c9dd54f7e3c12e6e7a04032c17df531ac4d600

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -6,23 +6,27 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+       /* we abadon the decimals,means: we just transfer the  part of interger*/
+     if(a&80000000==0)
+	return (a>>16)&0x7FFF;
+     else
+	return -((a>>16)&0x7FFF);
 }
 
 static inline FLOAT int2F(int a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	return (FLOAT)a*65536;
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
 	nemu_assert(0);
-	return 0;
+	return (FLOAT)(a*b);
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
 	nemu_assert(0);
-	return 0;
+	return (FLOAT)(a/b);
 }
 
 FLOAT f2F(float);
