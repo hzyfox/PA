49b932df7236e476e63bc75e65591df2c2d63c7b
tracer-ics2015 <tracer@njuics.org>
2015-11-02 20:34:52 +0800

    >  compile NEMU 201315035 fox Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  20:34:52 up  4:07,  2 users,  load average: 0.24, 0.22, 0.23 11856d769a84720a8066a92a903b182ea02adca2

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,7 +3,13 @@
 #define instr sub
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val - op_src->val;
+    DATA_TYPE result;
+if(op_src->size==1&&(op_dest->size==2||op_dest->size==4)){
+	 result = op_dest->val - (DATA_TYPE_S)op_src->val;
+}
+else
+     result = op_dest->val - op_src->val;
+
 	OPERAND_W(op_dest, result);
 
 	int dest=(int)op_dest->val;
