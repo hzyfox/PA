08dc71904e605deac8169f0dfb2c7ebd8c949bb8
tracer-ics2015 <tracer@njuics.org>
2015-11-10 10:04:06 +0800

    >  compile NEMU 201315035 fox Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  10:04:06 up  3:22,  2 users,  load average: 0.37, 0.25, 0.25 65faf4044c44c9adf56c1b9878f2af75cad1be2

--- nemu/src/cpu/exec/jump/call-template.h
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -3,19 +3,18 @@
 #define instr call
 
 
-
 static void do_execute(){
-
+   int len=concat4(decode_, i, _, SUFFIX)(cpu.eip+1)+1;
 
     #if DATA_BYTE==2
    cpu.esp-=4;
-   MEM_W(cpu.esp,cpu.eip);
+   MEM_W(cpu.esp,cpu.eip+len);
 
    cpu.eip=(op_src->val+cpu.eip)&0x0000FFFF;
 
    #else
    cpu.esp-=4;
-   MEM_W(cpu.esp,cpu.eip);
+   MEM_W(cpu.esp,cpu.eip+len);
 
    cpu.eip+=op_src->val;
    #endif // DATA_BYTE
@@ -26,5 +25,25 @@ static void do_execute(){
 }
 
 make_instr_helper(i)
-make_instr_helper(rm)
+make_helper(concat5(instr, _, rm, _, SUFFIX)){
+int len=concat4(decode_, rm, _, SUFFIX)(cpu.eip+1)+1;
+ #if DATA_BYTE==2
+   cpu.esp-=4;
+   MEM_W(cpu.esp,cpu.eip+len);
+
+   cpu.eip=(op_src->val+cpu.eip)&0x0000FFFF;
+
+   #else
+   cpu.esp-=4;
+   MEM_W(cpu.esp,cpu.eip+len);
+
+   cpu.eip+=op_src->val;
+   #endif // DATA_BYTE
+
+   print_asm_template1();
+   return len;
+
+
+
+}
 #include "cpu/exec/template-end.h"
